f8ddf5704ded0e9787010643b425d98c3f0e4a5e
imraj560 <raju560.webdev@gmail.com>
2024-01-15 10:08:20 +0600

    db setup success

--- backend/Server.js
+++ backend/Server.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/Workout');
 
 //lets instantiate express
 
 const app = express();
 
-//now listen to this port
+// //now listen to this port
 
-app.listen(process.env.PORT, ()=>{
+// app.listen(process.env.PORT, ()=>{
 
-    console.log('listening to home made port', process.env.PORT);
-})
+//     console.log('listening to home made port', process.env.PORT);
+// })
 
 
 //default middleware
@@ -26,3 +27,16 @@ app.use((req, res, next)=>{
 
 //routes
 app.use('/api/workout', workoutRoutes);
+
+//Database connect
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+
+    app.listen(process.env.PORT, ()=>{
+
+        console.log('Connected to DB, port no:', process.env.PORT);
+    })
+
+}).catch((error)=>{
+
+    console.log(error);
+})
