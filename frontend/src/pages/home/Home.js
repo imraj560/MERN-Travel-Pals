bf5c14f2b460bbe0f4de8c431012963ade9f1a76
imraj560 <raju560.webdev@gmail.com>
2024-01-19 11:25:42 +0600

    Intial pages created and set

---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,38 @@
+import './Home.css';
+
+const Home = ()=>{
+
+    return (
+        <>
+
+         <div id="banner">
+            <p>What your friends are Working on?</p>
+        </div>
+
+         <section id="day_title">
+            <p>
+                Thursday 18th January
+            </p>
+        </section>
+
+        <section id="workout_profiles">
+
+            <div>
+                 
+            </div>
+             <div>
+                 
+            </div>
+            <div>
+                 
+            </div>
+
+        </section>
+
+        </>
+       
+       
+    )
+}
+
+export default Home;
