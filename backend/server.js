03064228008ebf63caff1e522b37f6292f81a2ca
imraj560 <raju560.webdev@gmail.com>
2024-02-14 17:56:01 +0600

    CORS new setup

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workoutRouter');
 const userRoutes = require('./routes/userRouter');
+const cors = require('cors')
 const app = express();
 /**Some more changes */
+
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
 //default middleware
 app.use(express.json());
+
+
 app.use((req, res, next)=>{
 
     console.log(req.path, req.method);
     next();
 })
+
+app.use(cors(corsOptions));
+
 
 
 //routes for workout api
